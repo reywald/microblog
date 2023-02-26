7995ddebfae6a5e723fa1fb3fcf61e22f09e1df6
Agbarakwe Ikechukwu Samuel <37699249+reywald@users.noreply.github.com>
2023-02-26 12:07:57 +0000

    Combining Own and Followed Posts

--- app/models.py
+++ app/models.py
@@ -50,9 +50,11 @@ class User(UserMixin, db.Model):
       self.followed.remove(user)
 
   def followed_posts(self):
-    return Post.query.join(
+    followed = Post.query.join(
       followers, (followers.c.followed_id == Post.user_id)).filter(
         followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+    own = Post.query.filter_by(user_id == self.id)
+    return followed.union(own).order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
